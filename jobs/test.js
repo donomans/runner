8538703622ead11e21ee4dfebbe74f1b44f48e3a
Donovan Sandey <fronn1@gmail.com>
2013-08-28 18:29:14 -0500

    some more initial files
    scoping out the basic structure more

---
+++ jobs/test.js
@@ -0,0 +1,8 @@
+export.config.test = {
+    /* add configuration here for a test task/job */
+}
+
+/// this is a test task/job
+export.Run = function() {
+  ///Run the task
+}
--- runner.js
+++ runner.js
@@ -1,3 +1,32 @@
 /**
  * main file to start server and
  */
+
+var config = {};
+
+
+var connect = require('connect')
+var http = require('http')
+
+var connect = require('connect'), http = require('http');
+
+var app = connect()
+    .use(connect.favicon())
+    .use(connect.logger('dev'))
+    .use(connect.static('site'))
+    .use(connect.directory('site'))
+    .use(connect.cookieParser());
+
+http.createServer(app).listen('8080','localhost');
+
+
+
+
+setTimeout(1, function(){
+  ///start the work of finding all the jobs and organize them
+  
+  
+  setTimeout(1, function(){
+    //start it the first job, potentially
+  });
+});
